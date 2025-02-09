698e0241ee109f66cceff9516d7c78e59a3084ac
Test Qlgo <testqlgo@gmail.com>
2025-02-10 01:15:52 +0530

    Add Suspense for loading state in Home component and improve API URL handling

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import IndexCard from "@/components/cards/IndexCard";
+import { Suspense } from "react";
 
 
 export default async function Home() {
 
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000' 
     const response = await fetch(`${apiUrl}/indexdata`);
     const data = await response.json();
 
@@ -12,7 +13,9 @@ export default async function Home() {
     <div className="pt-3">
       <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {data.map((item,i)=>(
-          <IndexCard key={i} data={item}/>
+          <Suspense key={i} fallback={<p>loading</p>}>
+          <IndexCard data={item}/>
+          </Suspense>
         ))}
 
       </div>
